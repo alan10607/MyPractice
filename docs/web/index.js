3d37792b78f84d7a25396236fc35e89554e280a4
alan10607 <hiden1414@gmail.com>
2023-10-05 14:23:48 +0800

    update web

--- docs/web/index.js
+++ docs/web/index.js
@@ -3,10 +3,11 @@ const FILE_URL = [
     "https://api.github.com/repos/alan10607/MyPractice/contents/src/leetCode/java"
 ];
 const LEETCODE_URL = "https://leetcode.com/problems/";
-const LOAD_SIZE = 10;
+var LOAD_SIZE = 10;
 var CODE = new Array(FILE_URL.length).fill(null).map(() => new Map());
 var LOAD_CNT = FILE_URL.length;
 var SOU_NO = [];
+const STORAGE_KEY = "lc-did-no";
 
 function get(url, callback, ...args){
 	$.ajax({
@@ -70,14 +71,16 @@ function printPages(){
     }
     SOU_NO = Array.from(noSet).sort((a, b) => a - b);
 
-    $("#page-bar").empty().text("題號: ");
+    $("#page-bar").empty();
+    $("<span>", {text : `題數: ${LOAD_SIZE}`, "onclick" : "changeLoadSize()"}).appendTo($("#page-bar"));
     for(let i=0; i<SOU_NO.length; ++i){
         if(i % LOAD_SIZE == LOAD_SIZE - 1 || i == SOU_NO.length - 1){
             var start = i - (LOAD_SIZE > 1 && i % LOAD_SIZE == 0 ? LOAD_SIZE : i % LOAD_SIZE);
-            var str = SOU_NO[start] + "-" + SOU_NO[i];
+            var str = start === i ? SOU_NO[start] : SOU_NO[start] + "-" + SOU_NO[i];
             $("<span>", {text : str, "onclick" : `printCode(${start}, LOAD_SIZE); getBold(this)`}).appendTo($("#page-bar"));
         }
     }
+    $("<span>", {text : "[每日一題]", "onclick" : "printRandom()"}).appendTo($("#page-bar"));
 }
 
 function printCode(start, len){
@@ -88,7 +91,9 @@ function printCode(start, len){
         var no = SOU_NO[start + i];
         var problem = PROBLEMS.get(no);
         $("<div>", {id : no}).appendTo($("#code-box"));
-        $("<a>", {text : no, href : "#" + no}).appendTo($("#no-bar"));
+        if(len > 1) {
+            $("<a>", {text : no, href : "#" + no}).appendTo($("#no-bar"));
+        }
         $("<span>", {text : no + ". "}).appendTo($("#" + no));
         $("<a>", {text : problem, href : LEETCODE_URL + problem, target : "_blank"}).appendTo($("#" + no));
         for(let code of CODE){
@@ -118,6 +123,57 @@ function getBold(e){
     $(e).addClass("bold");
 }
 
+function changeLoadSize(){
+    const queue = [10, 1, 20];
+    const index = queue.indexOf(LOAD_SIZE);
+    LOAD_SIZE = queue[(index + 1) % queue.length];
+    printPages();
+}
+
+function t(){
+for(let i=0; i<230; ++i){
+    printRandom();
+}
+}
+function printRandom(){
+    $("#random-bar").empty();
+    const didNos = getDidNos();
+    $("<span>", {text : `歷史: ${getDidNos()}`}).appendTo($("#random-bar"));
+    
+    if(didNos.length < SOU_NO.length){
+        const no = getRandomProblemNo();
+        const problemUrl = `https://leetcode.com/problems/${PROBLEMS.get(no)}`;
+        $("<span>", {text : `本題: ${no}`}).appendTo($("#random-bar"));
+        $("<a>", {text : problemUrl, "href" : problemUrl, "target" : "_blank"}).appendTo($("#random-bar"));
+    }else{
+        $("<span>", {text : "已完成全部"}).appendTo($("#random-bar"));
+    }
+
+    $("<span>", {text : "[清除紀錄]", "onclick" : `saveDidNos([]); $("#random-bar").empty()`}).appendTo($("#random-bar"));
+}
+
+function getDidNos(){
+    const storage = localStorage.getItem(STORAGE_KEY);
+    return storage ? JSON.parse(storage) : [];  
+}
+
+function saveDidNos(didNos){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(didNos));  
+}
+
+function getRandomProblemNo(){
+    const noSet = new Set(SOU_NO);
+    const didNos = getDidNos();  
+    console.log("Did Solutions:", didNos);
+    for(let didNo of didNos){
+        noSet.delete(didNo);
+    }
+    const noArray = Array.from(noSet);
+    const no = noArray[Math.floor(Math.random() * noArray.length)];
+    didNos.push(no);
+    saveDidNos(didNos);
+    return no;
+}
 
 /* --- Loading視窗 --- */
 function showLoading(){
