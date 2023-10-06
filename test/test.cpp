6f5b7a3fc71b2d08f2d757bcb769f577b812e876
alan10607 <hiden1414@gmail.com>
2023-10-06 18:29:34 +0800

    add c++ test

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int test() {
+        return 0;
+    }
+};
+
+int main() {
+    Solution s;
+    int res = s.test();
+    cout << res << endl;
+    cout << "---end---" << endl;
+}
