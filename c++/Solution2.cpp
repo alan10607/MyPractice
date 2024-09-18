f3282ed090055f8f56a8d9cf62041e1e7ae889d6
alan10607 <alan10607.dev@gmail.com>
2024-09-18 17:16:53 +0800

    review 2

--- c++/Solution2.cpp
+++ c++/Solution2.cpp
@@ -3,21 +3,24 @@ class Solution2 {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode(-1);
-        ListNode* cur = dummy;
+        ListNode* head = dummy;
         int carry = 0;
-        while(l1 || l2){
-            int val1 = l1 ? l1->val : 0;
-            int val2 = l2 ? l2->val : 0;
-            int sum = val1 + val2 + carry;
+        while(l1 || l2 || carry) {//loop till carry and all listNodes are null
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+            int sum = v1 + v2 + carry;
             carry = sum / 10;
-            cur->next = new ListNode(sum % 10);
+            head->next = new ListNode(sum % 10);
 
-            cur = cur->next;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+            head = head->next;
+            if(l1) {
+                l1 = l1->next;
+            }
+            if(l2) {
+                l2 = l2->next;
+            }
         }
 
-        if(carry > 0) cur->next = new ListNode(carry);//記得加入最後
         return dummy->next;
     }
 };
