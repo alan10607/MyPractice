81d720956ac252a6c08e5961142b0b5ab71cfb4b
alan10607 <alan10607.dev@gmail.com>
2024-09-21 22:51:20 +0800

    review 11

--- c++/Solution11.cpp
+++ c++/Solution11.cpp
@@ -4,8 +4,9 @@ public:
     int maxArea(vector<int>& height) {
         int l = 0, r = height.size() - 1, res = 0;
         while(l < r){
-            res = max(res, min(height[l], height[r]) * (r - l));
-            if(height[l] < height[r]){
+            int area = min(height[l], height[r]) * (r - l);
+            res = max(res, area);
+            if(height[l] < height[r]){//水的容量由最小的高度決定, 所以優先去掉小的那一端
                 ++l;
             }else{
                 --r;
