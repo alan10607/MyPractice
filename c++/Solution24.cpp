153311962956149020e0feee2207c6ae6d18be75
alan10607 <alan10607.dev@gmail.com>
2024-09-24 19:10:52 +0800

    review 25

--- c++/Solution24.cpp
+++ c++/Solution24.cpp
@@ -4,9 +4,29 @@ public:
     ListNode* swapPairs(ListNode* head) {
         if(!head || !head->next) return head;
 
-        ListNode* next = head->next;
-        head->next = swapPairs(next->next);
-        next->next = head;
-        return next;//已經變成第一個了
+        ListNode* second = head->next;
+        head->next = swapPairs(second->next);
+        second->next = head;
+        return second;//已經變成第一個了
+    }
+};
+
+
+//LinkedList O(n) O(1)
+class Solution24_2 {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* tail = dummy;
+        while (tail->next && tail->next->next) {
+            ListNode* a = tail->next;
+            ListNode* b = tail->next->next;
+            a->next = a->next->next;
+            b->next = a;
+            tail->next = b;
+            tail = a;
+        }
+        return dummy->next;
     }
 };
--- c++/Solution25.cpp
+++ c++/Solution25.cpp
@@ -2,41 +2,107 @@
 class Solution25 {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* dummy = new ListNode(-1, head);
-        ListNode* start = dummy;
-        ListNode* cur = start->next;
-        for(int i=1; cur; ++i){
-            cur = cur->next;
-            if(i % k == 0)
-                start = reverse(start, cur);//翻轉start~end間(皆不包含)
+        ListNode* end = head;
+        for (int i = 0; i < k; ++i) {
+            if (!end) {
+                return head;
+            } else {
+                end = end->next;
+            }
+        }
 
+        ListNode* new_head = reverse(head, end);
+        head->next = reverseKGroup(end, k); //此時head已經是最後一個
+        return new_head;
+    }
+
+    ListNode* reverse(ListNode* start, ListNode* end) {
+        ListNode* pre = end;
+        ListNode* cur = start;
+        while (cur != end) {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre; // 此時變成第一個了
+    }
+};
+/* 這題也可用recursive的方式做(類似24題), if k=3
+1 -> 2 -> 3 -> 4
+start          end
+cur            pre
+
+---------------v
+1    2 -> 3 -> 4
+pre  cur
+
+
+---------------v
+1 <- 2    3 -> 4
+     pre  cur
+
+---------------v
+1 <- 2 <- 3    4
+          pre  cur(此時cur=end, 回傳pre作為new_head)
+*/
+
+//LinkedList O(n) O(1)
+class Solution25_2 {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* start = dummy;
+        ListNode* end = head;
+        for(int i =1  ;end;  ++i){
+            end = end->next;
+            if (i % k == 0) {
+                start = reverse(start, end);//翻轉start~end間(皆不包含)
+            }
         }
         return dummy->next;
     }
 
     ListNode* reverse(ListNode* start, ListNode* end) {
-        ListNode* pre = end;
         ListNode* cur = start->next;
-        ListNode* nextStart = start->next;//用來回傳
-        while(cur != end){
+        ListNode* pre = end;
+        ListNode* next_start = start->next;//用來回傳
+        while (cur != end) {
             ListNode* next = cur->next;
-            cur->next= pre;
+            cur->next = pre;
             pre = cur;
             cur = next;
         }
         start->next = pre;
 
-        return nextStart;
+        return next_start;
     }
 };
-/*
--1 -> 1 -> 2 -> 3 -> 4
- s    c
- s              c=end
 
- ----------v
--1    1 <- 2    3 -> 4
-      ----------^
-      s         c
+/* if k=3
+-1 -> 1 -> 2 -> 3 -> 4 -> 5
+ start               end
+      next_start
+      cur            pre
+
+      ---------------v
+-1 -> 1    2 -> 3 -> 4 -> 5
+      pre  cur
+
+      ---------------v
+-1 -> 1 <- 2    3 -> 4 -> 5
+           pre  cur
+
+      ---------------v
+-1 -> 1 <- 2 <- 3    4 -> 5
+                pre  cur(此時cur=end, 離開while)
+
+      ---------------v
+-1    1 <- 2 <- 3    4 -> 5
+ ---------------^
+ start          pre
+
+start接回pre, 並回傳next_start作為下次的start
 
 */
