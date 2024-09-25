831dac3e67deebc2f5779de205061c2d32289888
alan10607 <alan10607.dev@gmail.com>
2024-09-25 18:52:57 +0800

    review 32

--- c++/Solution31.cpp
+++ c++/Solution31.cpp
@@ -2,21 +2,35 @@
 class Solution31 {
 public:
     void nextPermutation(vector<int>& nums) {
-        //must be in place and use only constant extra memory
+        // must be in place and use only constant extra memory
         int i = nums.size() - 2, j = nums.size() - 1;
 
-        while(i >= 0 && nums[i] >= nums[i + 1]) --i;//等於也略過, 找強制遞減
+        while (i >= 0 && nums[i] >= nums[i + 1]) { // 等於也略過, 找強制遞減
+            --i;
+        }
 
-        if(i >= 0){//若i=-1, 表示為全遞減, 此時為最大, 跳過直接reverse成最小
-            while(i < j && nums[i] >= nums[j]) --j;
+        if (i >= 0) { // 若i=-1, 表示為全遞減, 此時為最大, 跳過直接reverse成最小
+            while (j > i && nums[j] <= nums[i]) {
+                --j;
+            }
             swap(nums[i], nums[j]);
         }
 
-        reverse(nums.begin() + i + 1, nums.end());//i+1到end
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
 /*
-由後往前找某數, 該數之後為降階數列
+順序:
+1234
+1243
+1324
+1342
+4123
+4231
+4321
+
+ex 1:
+由後往前找某數, 該數的後面比自己大
 1   2   7   3   2   1
     i
 
@@ -28,7 +42,24 @@ swap(nums[i], nums[j])
 1   3   7   2   2   1
         -   -   -   -
 
-重新sort(剛好是reverse)
+重新sort i 之後的位置, 由於是遞減數列所以可以剛好用reverse
 1   3   1   2   2   7
+
+
+ex 2 重複數字的情況:
+由後往前找某數, 該數的後面比自己大
+1   2   3   3
+    i
+
+由後往前找第一個大於該數的數字
+1   2   3   3
+    i       j
+
+swap(nums[i], nums[j])
+1   3   3   2
+        _   _
+
+reverse i 之後的位置
+1   3   2   3
 
 */
--- c++/Solution32.cpp
+++ c++/Solution32.cpp
@@ -1,5 +1,91 @@
-//Stack O(n) O(n)
+//Greedy O(n) O(1)
 class Solution32 {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            s[i] == '(' ? ++left : ++right;
+            if (left == right) {
+                res = max(res, right * 2); // left=right, 相同數量*2
+            } else if (left < right) {
+                left = right = 0;
+            }
+        }
+
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            s[i] == '(' ? ++left : ++right;
+            if (left == right) {
+                res = max(res, right * 2);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return res;
+    }
+};
+/* 不用空間的解法, 透過是貪心算法計算兩種括號的數量, left, right分別代表左右括號, i
+代表index
+
+ex:
+0   1   2   3   4   5   6
+(   )   (   (   )   (   )
+由左往右-->
+i=0, left=1, right=0
+i=1, left=1, right=1, l=r成對, max+len=2
+i=2, left=2, right=1
+i=3, left=3, right=1
+i=4, left=3, right=2
+i=5, left=4, right=2
+i=6, left=4, right=3
+
+由右往左<--
+i=6, left=0, right=1
+i=5, left=1, right=1, l=r成對, max+len=2
+i=4, left=1, right=2
+i=3, left=2, right=2, l=r成對, max+len=4
+i=2, left=3, right=2, l>r不合理要歸零, left=right=0
+i=1, left=0, rigth=1
+i=0, left=1, right=1, l=r成對, max+len=2
+得res=4
+
+可見需要兩次左右來訪,
+左往右解決右括號較多的情況, ex: ())
+右往左解決左括號較多的情況, ex: (()
+*/
+
+
+//Stack O(n) O(n)
+class Solution32_2 {
+public:
+    int longestValidParentheses(string s) {
+        int res = 0;
+        stack<int> st;
+        st.push(-1);//用來當作最左, 方便計算
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == ')' && st.top() >= 0 && s[st.top()] == '(') { //判斷是否成對, 需注意st.top()=-1時要跳過
+                st.pop();
+                res = max(res, i - st.top());
+            } else {
+                st.push(i);
+            }
+        }
+
+        return res;
+    }
+};
+/*
+    (   )   (   (   (   )   )
+-1
+    0---1  len=1-(-1)=2
+            2
+                3-----------6  len=6-2=4
+                    4---5  len=5-3=2
+
+*/
+
+//Stack O(n) O(n)
+class Solution32_3 {
 public:
     int longestValidParentheses(string s) {
         stack<int> st;//<'('的位置
@@ -20,12 +106,3 @@ public:
         return res;
     }
 };
-/*
-    (   )   (   (   (   )   )
--1
-    0---1  len=1-(-1)=2
-            2
-                3-----------6  len=6-2=4
-                    4---5  len=5-3=2
-
-*/
